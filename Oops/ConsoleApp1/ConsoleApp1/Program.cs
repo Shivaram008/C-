82bfd58d69823653d57c2ccd3c199d8bec1f003c
Shivaram008 <ram.cheruku@cognine.com>
2021-05-24 15:49:43 +0530

    NamespaceEx

---
+++ Oops/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Display Method in ConsoleApp1 Main Namespace ");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Oops/ConsoleApp1/NamespaceDemo/Program1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+namespace NamespaceDemo
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Program.Display();
+        }
+    }
+}
